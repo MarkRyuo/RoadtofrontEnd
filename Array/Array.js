5f191134e9193a7629cb1e7e5622e5b82cb311de
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-29 08:15:50 +0800

    update herE

--- Array/Array.js
+++ Array/Array.js
@@ -1,5 +1,13 @@
 // Todo Array = a variable like structure that can hold more than 1 value  
 
-let listNames = ["Ryuo", "Moda", "Mark", "Nicole", "Samantha", "Sopheya"] ;
+// let listNames = ["Ryuo", "Moda", "Mark", "Nicole", "Samantha", "Sopheya"] ;
 
-console.log(listNames[1]) ;
+// console.log(listNames[1]) ;
+
+let number = ["one", "two", "three", "four", "five"] ;
+
+numberOf(number) ;
+
+function numberOf(number) {
+  
+}

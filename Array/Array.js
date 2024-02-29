4e0fb385905b0e40d5c4e184a90ddfd922d27512
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-29 08:12:55 +0800

    udpate herE

--- Array/Array.js
+++ Array/Array.js
@@ -1,2 +1,5 @@
 // Todo Array = a variable like structure that can hold more than 1 value  
 
+let listNames = ["Ryuo", "Moda", "Mark", "Nicole", "Samantha", "Sopheya"] ;
+
+console.log(listNames[1]) ;

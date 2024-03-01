39481f4aa9e1911eaaf14cab4d5d3b5986891117
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-01 20:03:22 +0800

    udpaet ehrE

--- ALL.js
+++ ALL.js
@@ -5,5 +5,12 @@ const USERNAME = ["Marck", "Ryuo", "Moda", "Arck", "Mak", "Jhon Mark"];
 nameOfCharacter(USERNAME) ;
 
 function nameOfCharacter(USERNAME) {
+
+  let mc = 0 ;
   
+  switch(USERNAME){
+    case mc == 0 :
+      console.log(USERNAME[0]) ;
+      break ;
+  }
 }

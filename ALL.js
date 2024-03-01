f29cccb5504ca5854c9ed993712c69c96f6b403f
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-01 20:18:31 +0800

    udpaet ehrE

--- ALL.js
+++ ALL.js
@@ -1,29 +1,29 @@
 // Todo Array 
 
 const USERNAME = ["Marck", "Ryuo", "Moda", "Arck", "Mak", "Jhon Mark"];
-let mc = 2 ;
+let mc = 4 ;
 nameOfCharacter(USERNAME, mc) ;
 
 function nameOfCharacter(USERNAME,mc) {
   
   switch(true){
     case mc == 0 :
-      console.log(USERNAME[0]) ;
+      console.log(`Your ${USERNAME[0]}, you put number ${mc}!`) ;
       break ;
     case mc == 1 :
-      console.log(USERNAME[1]) ;
+      console.log(`Your ${USERNAME[1]}, you put number ${mc}!`) ;
       break ;
     case mc == 2 :
-      console.log(USERNAME[2]) ;
+      console.log(`Your ${USERNAME[2]}, you put number ${mc}!`) ;
       break ;
     case mc == 3 :
-      console.log(USERNAME[3]) ;
+      console.log(`Your ${USERNAME[3]}, you put number ${mc}!`) ;
       break ;
     case mc == 4 :
-      console.log(USERNAME[4]) ;
+      console.log(`Your ${USERNAME[4]}, you put number ${mc}!`) ;
       break ;
     case mc == 5 : 
-      console.log(USERNAME[5]) ;
+      console.log(`Your ${USERNAME[5]}, you put number ${mc}!`) ;
       break ;
     default :
       console.log(mc, " Character is not found in the verse") ;

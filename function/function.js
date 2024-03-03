ba8d21d50206c57c604612178ec93de54bd5590d
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-03 17:05:52 +0800

    update herE

--- function/function.js
+++ function/function.js
@@ -44,7 +44,7 @@ happyBirthday("Happy Birthday!") ;
 
 function happyBirthday(birth) {
 
-  for(let i = 0 ; i <= 10 ; i+=1 ) {
+  for(let i = 0 ; i < 10 ; i+=1 ) {
     i += birth ;
     return i ;
   }

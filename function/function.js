12089c97ff68a72c674f73f73aa1ba008f79b47a
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-03 13:06:52 +0800

    udpate herE

--- function/function.js
+++ function/function.js
@@ -1,10 +1,10 @@
 
-              // Parameter
+            // Parameter
 function names (name) {
 
   console.log(name) ;
 } ;
 
-    // Argument
-names("Jhon Mark")
+      // Argument
+names("Jhon Mark") ; // Call the function 
 

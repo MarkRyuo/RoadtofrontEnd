e5fc19d6c3a07f59def175cb2aa8e5611cb89610
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-07 13:33:45 +0800

    udpate ehrE

--- function/function2.js
+++ function/function2.js
@@ -1,10 +1,12 @@
-let num1 = "" ;
-let num2 = "" ;
+let num1 = "34" ;
+let num2 = "50" ;
 
 num1 = Number(num1) ; //Type conversion 
 num2 = Number(num2) ;
 
-addOf(num1, num2) ;
+let resultAdd = addOf(num1, num2) ;
+
+console.log(`Addition ${resultAdd}`) ;
 
 function addOf(num1, num2) {
 

3808f9a23064a8864fea84fcbe6b5c84a29f73c1
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-08 20:14:04 +0800

    update herE

--- function/function2.js
+++ function/function2.js
@@ -41,7 +41,7 @@ console.log(`${num1} * ${num2} = ${resultMulti}`) ;
 
 let divResult = divOf(num1, num2) ;
 
-console.log("")
+console.log(`${num1} / ${num2} = ${divResult}`)
 
 function divOf(num1, num2){
 
